77c9862b367a7cd29416a596a122146305ae28e2
root <root@localhost.localdomain>
2020-03-10 14:06:53 +0100

    Modified DB values in index.php to suit server

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 	mb_internal_encoding("UTF-8");
-	
+
 	session_start();
-	
+
 	/* Autoloader of classes */
 	function autoloadFunction($class)
 	{
@@ -13,16 +13,15 @@
 			require('model/' . $class . '.php');
 		}
 	}
-	
+
 	spl_autoload_register('autoloadFunction');
-	
+
 	/* Connecting to db */
-	Db::connect("server", "user", "password", "ossfs");
-	
+	Db::connect("localhost", "root", "database", "ossfs");
+
 	/* Save URL to array */
 	$router = new RouterController();
 	$router->process(array($_SERVER['REQUEST_URI']));
-	
+
 	/* Rendering of view */
 	$router->drawView();
-	
